24f790a999513aa0d82696a8d42d222bd54ede7c
Oleksii Shcherbak <qksardas001@mail.com>
2025-12-29 09:59:28 +0200

    Solution

---
+++ cinema/views.py
@@ -0,0 +1,11 @@
+from rest_framework import generics
+from .models import Movie
+from .serializers import MovieSerializer
+
+class MovieListCreateView(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
